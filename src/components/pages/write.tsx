61ab3359e082158b2ce9e5362147e4c63d22f37f
119786155 <119786155@qq.com>
2025-11-28 20:01:42 +0800

    fix: style of write page

--- src/components/pages/write.tsx
+++ src/components/pages/write.tsx
@@ -3,7 +3,7 @@ import { getContent } from '@/i18n'
 
 export const Write = () => {
   return (
-    <div className="mx-5 h-[calc(100dvh-60px)] overflow-hidden">
+    <div className="mx-5 h-dvh overflow-hidden">
       <Editor id="your_doc" placeholder={getContent('editor.placeholder')} />
     </div>
   )
