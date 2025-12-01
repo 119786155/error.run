3bb2f896d2cd5387ed2fc422101a6341d3b5aa87
119786155 <119786155@qq.com>
2025-12-01 21:48:12 +0800

    style of write page

--- src/components/pages/write.tsx
+++ src/components/pages/write.tsx
@@ -3,7 +3,7 @@ import { getContent } from '@/i18n'
 
 export const Write = () => {
   return (
-    <div className="px-1 min-w-dvw min-h-dvh fixed bg-(--background)">
+    <div className="px-1 overscroll-contain">
       <Editor id="your_doc" placeholder={getContent('editor.placeholder')} />
     </div>
   )
