ea2a484284dbf912d1038c2eee6241d31b708983
119786155 <119786155@qq.com>
2025-12-06 18:27:18 +0800

    rm the wrong emoji of flag

--- src/components/editor/plugins/emoji-kit.tsx
+++ src/components/editor/plugins/emoji-kit.tsx
@@ -1,13 +1,19 @@
 'use client'
 
+import type { Category, EmojiMartData } from '@emoji-mart/data'
 import emojiMartData from '@emoji-mart/data'
 import { EmojiInputPlugin, EmojiPlugin } from '@platejs/emoji/react'
-
 import { EmojiInputElement } from '@/components/ui/emoji-node'
+
+;(emojiMartData as EmojiMartData).categories.forEach((item: Category) => {
+  if (item.id !== 'flags') return
+
+  item.emojis = item.emojis.filter((emoji) => emoji !== 'flag-tw')
+})
 
 export const EmojiKit = [
   EmojiPlugin.configure({
-    //@ts-expect-error
+    // @ts-expect-error
     options: { data: emojiMartData },
   }),
   EmojiInputPlugin.withComponent(EmojiInputElement),
