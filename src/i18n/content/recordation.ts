5ff3db07b3f4e3752a42fb38daded96b17679815
119786155 <119786155@qq.com>
2026-04-22 22:07:28 +0800

    feat: use environment variables for recordation info

--- src/i18n/content/recordation.ts
+++ src/i18n/content/recordation.ts
@@ -1,3 +1,10 @@
-import R from '@/recordation.json'
-
-export const recordation = R.i18n
+export const recordation = {
+  psr: {
+    zh: import.meta.env.VITE_PSR_ZH,
+    en: import.meta.env.VITE_PSR_EN,
+  },
+  icp: {
+    zh: import.meta.env.VITE_ICP_ZH,
+    en: import.meta.env.VITE_ICP_EN,
+  },
+}
