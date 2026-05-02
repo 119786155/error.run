1ab8c10be83a0a888e12bd97a7ad224adf9f0dbd
119786155 <119786155@qq.com>
2026-05-03 01:18:19 +0800

    refactor: remove poem menu and update 404 page styling
    - Remove poem menu component and related styles
    - Update 404 page buttons with borderless line style
    - Add background animation to 404 page

--- src/components/pages/poem.tsx
+++ src/components/pages/poem.tsx
@@ -1,5 +1,4 @@
 import { Footer } from '@/components/footer'
-import { PoemMenu } from '@/components/ui/poem-menu'
 import { getContent } from '@/i18n'
 import '@/components/pages/poem.css'
 
@@ -11,7 +10,6 @@ export const Poem = () => {
 
   return (
     <div className="poem">
-      <PoemMenu />
       <div className="poem__content">
         <h1>{getContent('poem.title')}</h1>
         {paragraphs}
